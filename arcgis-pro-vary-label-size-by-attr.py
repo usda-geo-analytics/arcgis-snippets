31f40e1cbc550e2c51fdddaa8636ae4865721a80
USDA FPAC GEO Imagery Analytics Team <78555162+usda-geo-analytics@users.noreply.github.com>
2024-10-31 13:05:45 -0700

    Create arcgis-pro-vary-label-size-by-attr.py
    Build using Esri Living Atlas US States Generalized Boundaries layer

---
+++ arcgis-pro-vary-label-size-by-attr.py
@@ -0,0 +1,21 @@
+# Build using Esri Living Atlas US States Generalized Boundaries layer
+def FindLabel ([SQMI], [STATE_NAME]):
+    xs = "<FNT size = '10'>"
+    sm = "<FNT size = '14'>"
+    md = "<FNT size = '18'>"
+    lg = "<FNT size = '22'>"
+    xl = "<FNT size = '26'>"
+    
+    s = [SQMI]
+    n = [STATE_NAME]
+    
+    if s < 1000:
+        return xs + n + "</FNT>"
+    elif s < 10000:
+        return sm + n + "</FNT>"
+    elif s < 100000:
+        return md + n + "</FNT>"
+    elif s < 200000:
+        return lg + n + "</FNT>"
+    else:
+        return xl + n + "</FNT>"
